78e0ae2afe90f99ab31890b194c8d2061226fbc3
Emad Bsty <60713473+emadbsty@users.noreply.github.com>
2022-09-10 15:13:03 +0300

    Add files via upload

--- ANN-Lib/ANN.cs
+++ ANN-Lib/ANN.cs
@@ -9,122 +9,306 @@ namespace ANN_Lib
 {
     // This Neural Network Library Devloped by Emad Bsty Syria +963988553257
     // 
-   
-   
+
+
     public class ANN
     {
         //ANN(InputsX,Hidden Neurals,Output Neurals)
-        public float[] InputsX;
-        public Neural[] HN;
+        public double[] InputsX;
+        public List<Neural[]> HN;
         public Neural[] ON;
-        private float learnRate;
-        public float learnrate
+        private double learnRate;
+        ActivationFunction haf;
+        ActivationFunction oaf;
+        public double learnrate
         {
             get { return learnRate; }
             set { learnRate = value; }
         }
-        public ANN(int x, int h, int o)
+        public enum ActivationFunction
         {
-            learnRate = 0.5f;
-            InputsX = new float[x];
-            HN = new Neural[h];
+            Sigmoid,
+            ReLU,
+        }
+        public ANN(int x, int[] h, ActivationFunction haf, int o, ActivationFunction oaf)
+        {
+            learnRate = 0.6d;
+            InputsX = new double[x];
+            HN = new List<Neural[]>();
+            Neural[] hn;
             ON = new Neural[o];
-            for (int i = 0; i < h; i++)
+
+            this.haf = haf;
+            this.oaf = oaf;
+
+            for (int i = 0; i < h.Length; i++)
             {
-                HN[i] = new Neural(x);
+                hn = new Neural[h[i]];
+                HN.Add(hn);
+            }
+            for (int i = 1; i < HN.Count; i++)
+            {
+                for (int j = 0; j < HN[i].Length; j++)
+                {
+                    HN[i][j] = new Neural(HN[i - 1].Length);
+                }
+            }
+
+            for (int i = 0; i < HN[0].Length; i++)
+            {
+                HN[0][i] = new Neural(x);
             }
             for (int i = 0; i < o; i++)
             {
-                ON[i] = new Neural(h);
+                ON[i] = new Neural(HN[HN.Count - 1].Length);
             }
         }
-        public void Update(float[] Target)
+        public void Update(double[] Target)
         {
             for (int i = 0; i < ON.Length; i++)
             {
-                ON[i].Error = ON[i].y * (1.0f - ON[i].y) * (Target[i] - ON[i].y);
-                //ON[i].Error = ON[i].net * (1.0f - ON[i].net) * (Target[i] - ON[i].y);
+                if (oaf == ActivationFunction.ReLU)
+                {
+                    //ReLU
+                    ON[i].Error = DerivatReLU(ON[i].y) * (Target[i] - ON[i].y);
+                }
+                else
+                {
+                    // Segmoid
+                    ON[i].Error = ON[i].y * (1.0f - ON[i].y) * (Target[i] - ON[i].y);
+                }
             }
-
-            for (int i = 0; i < HN.Length; i++)
+            for (int i = 0; i < HN[HN.Count - 1].Length; i++)
             {
-                HN[i].Error = 0.0f;
+                HN[HN.Count - 1][i].Error = 0.0f;
                 for (int j = 0; j < ON.Length; j++)
                 {
-                    HN[i].Error += ON[j].w[i] * ON[j].Error;
+                    HN[HN.Count - 1][i].Error += ON[j].w[i] * ON[j].Error;
                 }
-                HN[i].Error = HN[i].Error * HN[i].y * (1.0f - HN[i].y);
-                //HN[i].Error = HN[i].Error * HN[i].net * (1.0f - HN[i].net);
+                if (haf == ActivationFunction.ReLU)
+                {
+                    //ReLU
+                    HN[HN.Count - 1][i].Error = HN[HN.Count - 1][i].Error * DerivatReLU(HN[HN.Count - 1][i].y);
+                }
+                else
+                {
+                    // Segmoid
+                    HN[HN.Count - 1][i].Error = HN[HN.Count - 1][i].Error * HN[HN.Count - 1][i].y * (1.0f - HN[HN.Count - 1][i].y);
+                }
+            }
+            for (int k = HN.Count - 2; k >= 0; k--)
+            {
+                for (int i = 0; i < HN[k].Length; i++)
+                {
+                    HN[k][i].Error = 0.0f;
+                    for (int j = 0; j < HN[k + 1].Length; j++)
+                    {
+                        HN[k][i].Error += HN[k + 1][j].w[i] * HN[k + 1][j].Error;
+                    }
+                    if (haf == ActivationFunction.ReLU)
+                    {
+                        //ReLU
+                        HN[k][i].Error = HN[k][i].Error * DerivatReLU(HN[k][i].y);
+                    }
+                    else
+                    {
+                        // Segmoid
+                        HN[k][i].Error = HN[k][i].Error * HN[k][i].y * (1.0f - HN[k][i].y);
+                    }
+                }
             }
             for (int j = 0; j < ON.Length; j++)
             {
                 for (int i = 0; i < ON[j].w.Length; i++)
                 {
-                    ON[j].w[i] = ON[j].w[i] + HN[i].y * ON[j].Error;
+                    ON[j].w[i] = ON[j].w[i] + learnRate * HN[HN.Count - 1][i].y * ON[j].Error;
                 }
-                ON[j].bias += ON[j].Error;
+                ON[j].bias += learnRate * ON[j].Error;
+            }
+            for (int k = 1; k < HN.Count; k++)
+            {
+                for (int i = 0; i < HN[k].Length; i++)
+                {
+                    for (int j = 0; j < HN[k][i].w.Length; j++)
+                    {
+                        HN[k][i].w[j] = HN[k][i].w[j] + learnRate * HN[k - 1][j].y * HN[k][i].Error;
+                    }
+                    HN[k][i].bias += learnRate * HN[k][i].Error;
+                }
+            }
+            for (int i = 0; i < HN[0].Length; i++)
+            {
+                for (int j = 0; j < HN[0][i].w.Length; j++)
+                {
+                    HN[0][i].w[j] = HN[0][i].w[j] + learnRate * InputsX[j] * HN[0][i].Error;
+                }
+                HN[0][i].bias += learnRate * HN[0][i].Error;
             }
 
-            for (int i = 0; i < HN.Length; i++)
-            {
-                for (int j = 0; j < HN[i].w.Length; j++)
-                {
-                    HN[i].w[j] = HN[i].w[j] + InputsX[j] * HN[i].Error;
-                }
-                HN[i].bias += HN[i].Error;
-            }
+            //********************** update other hidden 
         }
-        public float[] Process(float[] inputsx)
+        public void Process(double[] inputsx)
         {
             InputsX = inputsx;
-            float aux;
-            for (int i = 0; i < HN.Length; i++)
+            double aux;
+            //*******************************************
+            for (int i = 0; i < HN[0].Length; i++)
             {
-                aux = 0.0f;
-                for (int j = 0; j < HN[i].w.Length; j++)
+                aux = 0.0d;
+                for (int j = 0; j < HN[0][i].w.Length; j++)
                 {
-                    aux += InputsX[j] * HN[i].w[j];
+                    aux += InputsX[j] * HN[0][i].w[j];
                 }
-                aux += HN[i].bias;
-                HN[i].net = aux;
-                HN[i].y = sigmoid(aux);
+                aux += HN[0][i].bias;
+                HN[0][i].net = aux;
+                if (haf == ActivationFunction.ReLU)
+                {
+                    //ReLU
+                    HN[0][i].y = ReLU(aux);
+                }
+                else
+                {
+                    // Segmoid
+                    HN[0][i].y = sigmoid(aux);
+                }
             }
-            aux = 0.0f;
-            float[] yy = new float[ON.Length];
+            //*******************************************
+            for (int k = 1; k < HN.Count; k++)
+            {
+                for (int i = 0; i < HN[k].Length; i++)
+                {
+                    aux = 0.0d;
+                    for (int j = 0; j < HN[k][i].w.Length; j++)
+                    {
+                        aux += HN[k - 1][j].y * HN[k][i].w[j];
+                    }
+                    aux += HN[k][i].bias;
+                    HN[k][i].net = aux;
+                    if (haf == ActivationFunction.ReLU)
+                    {
+                        //ReLU
+                        HN[k][i].y = ReLU(aux);
+                    }
+                    else
+                    {
+                        // Segmoid
+                        HN[k][i].y = sigmoid(aux);
+                    }
+                }
+            }
+
+            //*******************************************
+            double[] yy = new double[ON.Length];
             for (int j = 0; j < ON.Length; j++)
             {
+                aux = 0.0f;
                 for (int i = 0; i < ON[j].w.Length; i++)
                 {
-                    aux += HN[i].y * ON[j].w[i];
+                    aux += HN[HN.Count - 1][i].y * ON[j].w[i];
                 }
                 aux += ON[j].bias;
                 ON[j].net = aux;
-                ON[j].y = sigmoid(aux);
+                if (oaf == ActivationFunction.ReLU)
+                {
+                    //ReLU
+                    ON[j].y = ReLU(aux);
+                }
+                else
+                {
+                    // Segmoid
+                    ON[j].y = sigmoid(aux);
+                }
                 yy[j] = ON[j].y;
             }
-            return yy;
+            //*******************************************
+            //return yy;
         }
-        public float sigmoid(float xx)
+        public double sigmoid(double xx)
         {
-            return (1.0f / (1.0f + (float)Math.Exp(-xx)));
+            return (1.0d / (1.0d + (double)Math.Exp(-xx)));
+        }
+        public double Derivatsigmoid(double xx)
+        {
+            return (sigmoid(xx) * (1.0d - sigmoid(xx)));
+        }
+        double aux = 0.02d;
+        public double ReLU(double xx)
+        {
+            if (xx > 0.0) return xx;
+            else return (aux * xx);
+        }
+        public double DerivatReLU(double xx)
+        {
+            if (xx > 0.0) return 1.0d;
+            else return aux;
+        }
+        public double[] SoftMax(double[] arr)
+        {
+            double sum = 0.0d;
+            double[] aux = new double[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                aux[i] = Math.Pow(Math.E, arr[i]);
+                sum += aux[i];
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                aux[i] = aux[i] / sum;
+            }
+            return aux;
+        }
+        public double DerivatSoftMax(int index, double[] arr)
+        {
+            double sum = 0.0d;
+            double[] aux = new double[arr.Length];
+            double po = 0.0d;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += Math.Pow(Math.E, arr[i]);
+            }
+            sum = sum * sum;
+            for (int i = 0; (i < arr.Length) && (i != index); i++)
+            {
+                po += Math.Pow(Math.E, arr[i]);
+            }
+            po = (arr[index] * po) / sum;
+            return po;
+        }
+        public double[] LogLoss(double[] y, double[] o)
+        {
+            double[] ll = new double[y.Length];
+            for (int i = 0; i < y.Length; i++)
+            {
+                ll[i] = -1.0d * y[i] * Math.Log10(o[i]) - (1.0d - y[i]) * Math.Log10(1.0d - o[i]);
+            }
+            return ll;
+        }
+        public double[] DerivatLogLoss(double[] y, double[] o)
+        {
+            double[] ll = new double[y.Length];
+            for (int i = 0; i < y.Length; i++)
+            {
+                ll[i] = -1.0d * y[i] * (1.0d / o[i]) - (1.0d - y[i]) * (1.0d / (1.0d - o[i]));
+            }
+            return ll;
         }
         public class Neural
         {
-            public float[] w;
-            public float bias;
-            public float Error;
-            public float net;
-            public float y;
+            public double[] w;
+            public double bias;
+            public double Error;
+            public double net;
+            public double y;
             static Random rand = new Random();
 
             public Neural(int inputs)
             {
-                w = new float[inputs];
+                w = new double[inputs];
                 for (int i = 0; i < inputs; i++)
                 {
-                    w[i] = ((float)rand.Next(-1000, 1000) / 2000.0f);
+                    w[i] = ((double)rand.Next(-3000, 3000) / 5000.0f);
                 }
-                bias = ((float)rand.Next(0, 2000) / 2000.0f);
+                bias = ((double)rand.Next(-2000, 2000) / 5000.0f);
             }
         }
     }
@@ -153,27 +337,24 @@ namespace ANN_Lib
             }
             learningRate = 0.5f;
         }
-        public void CalcDistance(float[] inputs)
+        public void CalcDistance(Color inputs)
         {
             for (int i = 0; i < dimension.Y; i++)
             {
                 for (int j = 0; j < dimension.X; j++)
                 {
                     nodes[j, i].Distance = nodes[j, i].Distance_Clac(inputs);
-                    float WRow = 0.0f;
-                    for (int k = 0; k < nodes[j, i].Weights.Length; k++)
-                    {
-                        WRow += nodes[j, i].Weights[k];
-                    }
-
-                    nodes[j, i].color = Color.FromArgb((int)(180 * WRow), (int)(180 * WRow), (int)(180 * WRow));
+                    nodes[j, i].color = Color.FromArgb((int)nodes[j, i].Weights[0]
+                        , (int)nodes[j, i].Weights[1]
+                        , (int)nodes[j, i].Weights[2]);
                 }
             }
-        }        
-        public SOM_Nodes Get_BMU()
+        }
+        public SOM_Nodes Get_BMU(Color inputs)
         {
             SOM_Nodes node = nodes[0, 0];
             float min_dis = nodes[0, 0].Distance;
+            int im = 0, jm = 0;
             for (int i = 0; i < dimension.Y; i++)
             {
                 for (int j = 0; j < dimension.X; j++)
@@ -182,19 +363,23 @@ namespace ANN_Lib
                     {
                         min_dis = nodes[j, i].Distance;
                         node = nodes[j, i];
+                        jm = j; im = i;
                     }
                 }
             }
+            nodes[jm, im].color = inputs;
+            nodes[jm, im].Distance = 0.0f;
+            node = nodes[jm, im];
             return node;
         }
-        public void Update(List<Point> neighboring, float[] inputs, float LR)
+        public void Update(List<Point> neighboring, Color inputs, float LR)
         {
             for (int i = 0; i < neighboring.Count; i++)
             {
-                for (int j = 0 ; j < inputs.Length;j++)
-                {
-                    nodes[neighboring[i].X, neighboring[i].Y].Weights[j] = nodes[neighboring[i].X, neighboring[i].Y].Weights[j] + LR * (inputs[j] - nodes[neighboring[i].X, neighboring[i].Y].Weights[j]);
-                }
+                nodes[neighboring[i].X, neighboring[i].Y].Weights[0] = nodes[neighboring[i].X, neighboring[i].Y].Weights[0] + LR * (inputs.R - nodes[neighboring[i].X, neighboring[i].Y].Weights[0]);
+                nodes[neighboring[i].X, neighboring[i].Y].Weights[1] = nodes[neighboring[i].X, neighboring[i].Y].Weights[1] + LR * (inputs.G - nodes[neighboring[i].X, neighboring[i].Y].Weights[1]);
+                nodes[neighboring[i].X, neighboring[i].Y].Weights[2] = nodes[neighboring[i].X, neighboring[i].Y].Weights[2] + LR * (inputs.B - nodes[neighboring[i].X, neighboring[i].Y].Weights[2]);
+                nodes[neighboring[i].X, neighboring[i].Y].color = Color.FromArgb((int)nodes[neighboring[i].X, neighboring[i].Y].Weights[0], (int)nodes[neighboring[i].X, neighboring[i].Y].Weights[1], (int)nodes[neighboring[i].X, neighboring[i].Y].Weights[2]);
             }
         }
         public List<Point> Get_neighboring(SOM_Nodes BMU,int radius)
@@ -267,16 +452,16 @@ namespace ANN_Lib
                 this.loc = loc;
                 for (int i = 0; i < w.Length; i++)
                 {
-                    w[i] = (float)rand.NextDouble() / 2.0f;
+                    w[i] = (float)(rand.NextDouble() * 255.0f);
                 }
             }
-            public float Distance_Clac(float[] inputs)
+            public float Distance_Clac(Color inputs)
             {
                 float dis = 0.0f;
-                for (int i = 0; i < inputs.Length; i++)
-                {
-                    dis = ((inputs[i] - this.w[i]) * (inputs[i] - this.w[i]));
-                }
+                dis += (inputs.R - this.w[0]) * (inputs.R - this.w[0]);
+                dis += (inputs.G - this.w[1]) * (inputs.G - this.w[1]);
+                dis += (inputs.B - this.w[2]) * (inputs.B - this.w[2]);
+                dis = dis / 3.0f;
                 return (float)Math.Sqrt(dis);
             }
         }
@@ -314,7 +499,7 @@ namespace ANN_Lib
                 for (int j = 0; j < data.GetLength(0); j++)
                 {
                     if (data[j, i] < 0.0f)
-                        data[j, i] = 0.0f;
+                        data[j, i] = 0.0f;                    
                 }
             }
             return data;
@@ -344,6 +529,7 @@ namespace ANN_Lib
         }
         public float[,] AddPadding(float[,] image, int Pad)
         {
+            
             float[,] aux = new float[image.GetLength(0) + 2 * Pad, image.GetLength(1) + 2 * Pad];
             for (int i = 0; i < aux.GetLength(0); i++)
             {
